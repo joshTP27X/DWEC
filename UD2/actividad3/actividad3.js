d8c35e20611de2aab2f2afb8dc5d45de64aabf04
Joseph Arizpe <josaricol@alu.edu.gva.es>
2023-09-27 19:28:14 +0200

    aegragamos avances del actividad 3 incompleta

---
+++ UD2/actividad3/actividad3.js
@@ -0,0 +1,7 @@
+//let fondo = document.getElementById("fondo").style.backgroundColor = ;
+//let color = parseInt(Math.random(256));
+//fondo.style.backgroundColor = color;
+function cambio(){
+    let cc = document.getElementById("fondo");
+    cc.style.backgroundColor = `#${parseInt(Math.random(256))}`;
+}
